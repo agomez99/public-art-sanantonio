23759a3f02bd9cd75143fe77b2cb30a79224c14e
Austine Gomez <austine.gomez@yahoo.com>
2023-07-22 20:37:55 -0500

    app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+ 
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence  mode='wait'>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
+  );
 }
+ 
+export default MyApp;
